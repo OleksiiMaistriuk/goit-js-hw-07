a4b02044e1642d42c0443edd5a6d1c0bcb07ede5
OleksiiMaistriuk <65893898+OleksiiMaistriuk@users.noreply.github.com>
2020-11-15 22:33:06 +0100

    сделал 6 задание

--- task-6.js
+++ task-6.js
@@ -1,1 +1,12 @@
+const findInput = document.querySelector('#validation-input');
+const findAtribute = document.querySelector('#data-length');
 
+findInput.addEventListener('change', e => {
+  if (findInput.selectionEnd === Number(findInput.dataset.length)) {
+    findInput.classList.add('valid');
+    findInput.classList.remove('invalid');
+  } else {
+    findInput.classList.add('invalid');
+    findInput.classList.remove('valid');
+  }
+});
