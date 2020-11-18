9a8f66bc4cabd08e9612b662b9e2b4b56fef40d8
OleksiiMaistriuk <65893898+OleksiiMaistriuk@users.noreply.github.com>
2020-11-18 20:34:09 +0100

    пофиксил 5 задание

--- task-5.js
+++ task-5.js
@@ -2,10 +2,8 @@ const inputFinde = document.querySelector('#name-input');
 const spanFinde = document.querySelector('#name-output');
 
 inputFinde.addEventListener('input', e => {
-  if (e.code === 'input') {
-    spanFinde.textContent = e.target.value;
-    e.target.value = '';
-  } else if (e.code !== 'input') {
+  spanFinde.textContent = e.target.value;
+  if (e.target.value === '') {
     spanFinde.textContent = 'незнакомец';
   }
 });
